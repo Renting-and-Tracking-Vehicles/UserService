bccfa71b8e1212be61ad157fea710b4eb4a50513
NikolinaPavkovic <73259509+NikolinaPavkovic@users.noreply.github.com>
2022-03-24 12:16:22 +0100

    feature/creating-model (#1)
    * feat: user model
    
    * feat: role model

---
+++ backend/src/main/java/com/example/userservice/model/Role.java
@@ -0,0 +1,11 @@
+package com.example.userservice.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class Role {
+    private int id;
+    private String name;
+}
