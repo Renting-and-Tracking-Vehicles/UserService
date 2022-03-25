5ce5248589cb611049a20f493e7b3017577cfcd4
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2022-03-25 09:45:33 +0100

    feat: database configuration

--- backend/src/main/java/com/example/userservice/model/Role.java
+++ backend/src/main/java/com/example/userservice/model/Role.java
@@ -4,8 +4,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
 @Getter @Setter @NoArgsConstructor
 public class Role {
+
+    @Id
+    @SequenceGenerator(name = "roleSeqGen", sequenceName = "roleSeqGen", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roleSeqGen")
+    @Column(name="id", unique=true, nullable=false)
     private int id;
+
+    @Column(name="name", nullable=false)
     private String name;
 }
