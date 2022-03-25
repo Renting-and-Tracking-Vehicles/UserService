de0f4893b348ffaa66668dd82dc5ccc4458828c0
NikolinaPavkovic <73259509+NikolinaPavkovic@users.noreply.github.com>
2022-03-25 09:48:44 +0100

    feat: creating model (#2)
    * feat: user model
    
    * feat: role model
    
    * feat: database configuration

---
+++ backend/src/main/java/com/example/userservice/model/RegisteredUser.java
@@ -0,0 +1,35 @@
+package com.example.userservice.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class RegisteredUser {
+    @Id
+    @SequenceGenerator(name = "userSeqGen", sequenceName = "userSeqGen", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userSeqGen")
+    @Column(name="id", unique=true, nullable=false)
+    private int id;
+    @Column(name="email", unique=true, nullable=false)
+    private String email;
+    @Column(name="password", nullable=false)
+    private String password;
+    @Column(name="name", nullable=false)
+    private String name;
+    @Column(name="surname", nullable=false)
+    private String surname;
+    @Column(name="phone", nullable=false)
+    private String phone;
+
+    public RegisteredUser(String email, String password, String name, String surname, String phone) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.phone = phone;
+    }
+}
